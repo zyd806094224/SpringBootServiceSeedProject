de66358b8863cdea03156fd2c0147738b16f3c92
zhaoyudong <zhaoyudong@staff.hexun.com>
2025-09-22 13:16:46 +0800

    springboot3.3.4版本、基础工程提交

---
+++ src/main/java/com/zyd/springbootserviceseedproject/SpringBootServiceSeedProjectApplication.java
@@ -0,0 +1,13 @@
+package com.zyd.springbootserviceseedproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServiceSeedProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootServiceSeedProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zyd/springbootserviceseedproject/SpringBootServiceSeedProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyd.springbootserviceseedproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootServiceSeedProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
